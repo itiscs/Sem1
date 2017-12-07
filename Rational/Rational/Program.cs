dc09375cf08cfd1b2555684752973ae46eab4191
moudroff <moudroff>
2017-12-07 11:19:14 +0300

    Rational

---
+++ Rational/Rational/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational r = new Rational(1,5);
+
+            r.Show();
+
+            r.SetChisl(3);
+
+            r.Show();
+
+            Rational r1 = new Rational(10,2);
+            Rational r2 = new Rational(1,3);
+
+
+            //r1.Show();
+            //r2.Show();
+            Console.WriteLine($"r1={r1}");
+            Console.WriteLine($"r2={r2}");
+
+            Rational r3 = new Rational();
+            r3.Sum(r1, r2);
+            
+            Console.WriteLine($"r1+r2={r3}");
+
+            Rational r4 = r3 + r2;
+            Console.WriteLine($"r3+r2={r4}");
+
+
+            //Console.WriteLine(r);
+        }
+    }
+}
---
+++ Rational/Rational/Rational.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational
+{
+
+    class Rational
+    {
+        private int chisl;
+        private int znam;
+
+        public Rational(int c, int z)
+        {
+            if (z != 0)
+            {
+                this.chisl = c;
+                this.znam = z;
+            }
+        }
+
+        public Rational(int c):this(c,1)
+        {
+            //this.chisl = c;
+            //this.znam = 1;
+        }
+
+        public Rational():this(0)
+        {
+            //this.chisl = 0;
+            //this.znam = 1;
+        }
+
+
+        public void SetChisl(int c)
+        {
+            this.chisl = c;
+        }
+
+        public int GetChisl()
+        {
+            return chisl;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{chisl}/{znam}");
+        }
+
+        public override string ToString()
+        {
+            if (znam == 1)
+                return $"{chisl}";
+
+            return $"{chisl}/{znam}";
+            
+        }
+
+        public static Rational operator+(Rational a, Rational b)
+        {
+            int c = a.chisl * b.znam + a.znam * b.chisl;
+            int z = a.znam * b.znam;
+            Rational r = new Rational(c, z);
+            return r;
+        }
+
+        public void Sum(Rational a, Rational b)
+        {
+            chisl = a.chisl * b.znam + a.znam * b.chisl;
+            znam = a.znam * b.znam;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

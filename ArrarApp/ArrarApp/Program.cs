da398267ae77a9ff6a001b01d71b97db06927108
moudroff <moudroff>
2018-10-08 10:01:32 +0300

    Array

--- ArrarApp/ArrarApp/Program.cs
+++ ArrarApp/ArrarApp/Program.cs
@@ -10,46 +10,32 @@ namespace ArrarApp
     {
         static void Main(string[] args)
         {
-            int n = 5, sum = 0;
-            int[] a = {1,2,3,4,5};
-            int[,] b = new int[n,n];
-           
+            int n = 100;
+            
+            //int[] a = {1,2,3,4,5,6,7};
+            int[] a = new int[n];
             Random r = new Random();
 
-            for (int i = 0; i < n; i++)
-                a[i] = r.Next(-100, 100);
-            //a[i] = int.Parse(Console.ReadLine);
-
-            int max = a[0], imax=0;
+            bool fl = true;
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine($"a[{i}] = {a[i],3}");
-                //sum += a[i];
-                if (a[i] > max)
-                {
-                    max = a[i];
-                    imax = i;
-                }
+                //a[i] = n - i;
+                //    a[i] = int.Parse(Console.ReadLine());
+                a[i] = r.Next(-100,100);
+                if (a[i] == 0)
+                    fl = false;
             }
-            Console.WriteLine($"sum={sum}");
 
-            //int s = 0;
-            //for (int i = 0; i < n; i++)
-            //    for (int j = 0; j < n; j++)
-            //    {
-            //        b[i, j] = r.Next(-100,100);
-            //        s += b[i, j];
-            //    }
-            //for (int i = 0; i < n; i++)
-            //{
-            //    for (int j = 0; j < n; j++)
-            //        Console.Write($"{b[i, j],4}");
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine($"s={s}");
-
-
-
+            int sum = 0;
+            int max = a[0];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($" {a[i]}\n");
+                sum += a[i];
+                if (a[i] > max)
+                    max = a[i];
+            }
+            Console.WriteLine($"Sum = {sum} Max = {max}");
 
         }
     }

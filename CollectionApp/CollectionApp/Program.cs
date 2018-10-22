60e9280ddedc890de87201ce6583ba009be6d488
moudroff <moudroff>
2018-10-22 09:54:48 +0300

    List+String

--- CollectionApp/CollectionApp/Program.cs
+++ CollectionApp/CollectionApp/Program.cs
@@ -10,6 +10,7 @@ namespace CollectionApp
     {
         static void Main(string[] args)
         {
+            //********************************матрицы*********************
             //int n = 10, m = 10;
             //int[,] a = new int[m, n];
 
@@ -34,30 +35,61 @@ namespace CollectionApp
             //    Console.WriteLine();
             //}
 
+            //****************************   Списки   **********************
+            //List<int> lst = new List<int>();
 
-            List<int> lst = new List<int>();
+            //Random r = new Random();
 
-            Random r = new Random();
+            //for(int i=0;i<20;i++)
+            //    lst.Add(i*11);
 
-            for(int i=0;i<4097;i++)
-                lst.Add(i*11);
+            ////lst.Sort();
+            ////lst.Remove(11);
 
-            //lst.Sort();
-            //lst.Remove(11);
+            ////for (int i = 0; i < lst.Count; i++)
+            ////    Console.WriteLine(lst[i]);
 
-            //for (int i = 0; i < lst.Count; i++)
-            //    Console.WriteLine(lst[i]);
-
-            lst.Reverse();
-            Console.WriteLine($"содержит 11? = {lst.Contains(11)}");
+            //lst.Reverse();
+            //Console.WriteLine($"содержит 11? = {lst.Contains(11)}");
             //lst.Distinct();
 
-            foreach (int item in lst)
+            //foreach (int item in lst)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            string s1 = "aac", s2 ="aaa";
+                        
+            Console.WriteLine(s1==s2 ? "да" : "нет");
+
+            //Console.WriteLine(s2.CompareTo(s1));
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("abc");
+            sb.Append("def");
+            //sb.Remove(2, 2);
+
+
+            s1 = sb.ToString();
+
+            Console.WriteLine(s1);
+
+            string s = Console.ReadLine();
+
+            //var lst = s.Split(',');
+            var l = new List<int>();
+
+            foreach (var str in s.Split(new char[]{',','.','/'}))
             {
-                //Console.WriteLine(item);
+                l.Add(int.Parse(str));
             }
 
-            
+            Console.WriteLine(l.Max());
+           
+
+
+
+
 
 
         }

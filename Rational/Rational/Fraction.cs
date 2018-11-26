3841eb96f1e7e333023465cb5879fd831cc99105
moudroff <moudroff>
2018-11-26 10:11:41 +0300

    ООП

---
+++ Rational/Rational/Fraction.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rational
+{
+    class Fraction
+    {
+        private int chisl;
+        private int znam;
+
+        public Fraction(int c, int z)
+        {
+            chisl = c;
+            znam = z;
+        }
+
+        public Fraction(int c)
+        {
+            chisl = c;
+            znam = 1;
+        }
+        public Fraction()
+        {
+            chisl = 0;
+            znam = 1;
+        }
+
+        public int GetChisl()
+        {
+            return chisl;
+        }
+
+        public int GetZnam()
+        {
+            return znam;
+        }
+
+        public void SetChisl(int c)
+        {
+            chisl=c;
+        }
+
+        public void SetZnam(int z)
+        {
+            if (z == 0)
+               Console.WriteLine("на ноль делить нельзя");
+            else
+                znam=z;
+        }
+
+        public override string ToString()
+        {
+            if (znam == 1)
+                return $"{chisl}";
+            else
+                return $"{chisl}/{znam}";
+        }
+
+
+
+
+    }
+
+
+}
--- Rational/Rational/Program.cs
+++ Rational/Rational/Program.cs
@@ -6,39 +6,56 @@ using System.Threading.Tasks;
 
 namespace Rational
 {
+   
     
     class Program
     {
         static void Main(string[] args)
         {
-            Rational r;
-            r = new Rational(1,0);
+            Fraction f1 = new Fraction();
+            Fraction f2 = new Fraction(5);
+            Fraction f = new Fraction(3,4);
+
+            Console.WriteLine($"f={f}");
+            Console.WriteLine($"f1={f1}");
+            Console.WriteLine($"f2={f2}");
+
+            f.SetChisl(6);
+
+            Console.WriteLine($"f={f}");
+
             
-            r.Show();
 
-            r.Chisl = 3;
+            //Rational r;
+            //r = new Rational(1,0);
 
-            r.Show();
+            //r.Show();
 
-            Rational r1 = new Rational(10,2);
-            Rational r2 = new Rational(-1,3);
+            //r.Chisl = 3;
 
+            //r.Show();
 
-            //r1.Show();
-            //r2.Show();
-            Console.WriteLine($"r1={r1}");
-            Console.WriteLine($"r2={r2}");
-
-            Rational r3 = new Rational();
-            r3.Sum(r1, r2);
-            
-            Console.WriteLine($"r1+r2={r3}");
-
-            Rational r4 = r3 + r2;
-            Console.WriteLine($"r3+r2={r4}");
+            //Rational r1 = new Rational(10,2);
+            //Rational r2 = new Rational(-1,3);
 
 
-            //Console.WriteLine(r);
+            ////r1.Show();
+            ////r2.Show();
+            //Console.WriteLine($"r1={r1}");
+            //Console.WriteLine($"r2={r2}");
+
+            //Rational r3 = new Rational();
+            //r3.Sum(r1, r2);
+
+            //Console.WriteLine($"r1+r2={r3}");
+
+            //Rational r4 = r3 + r2;
+            //Console.WriteLine($"r3+r2={r4}");
+
+
+            ////Console.WriteLine(r);
         }
     }
+
+    
 }

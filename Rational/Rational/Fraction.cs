d04f5cc61ba9716cf7146baecf95762113468b41
moudroff <moudroff>
2018-12-03 09:32:30 +0300

    OOP+static

--- Rational/Rational/Fraction.cs
+++ Rational/Rational/Fraction.cs
@@ -8,24 +8,70 @@ namespace Rational
 {
     class Fraction
     {
+        private static int kol = 0;
+        
         private int chisl;
         private int znam;
+
+        public int Chisl
+        {
+            get
+            {
+                return chisl;
+            }
+            set
+            {
+                chisl = value;
+                sokr();
+            }
+        }
+        
+        public int Znam
+        {
+            get
+            {
+                return znam;
+            }
+            set
+            {
+                if (value == 0)
+                    Console.WriteLine("Деление на ноль");
+                else
+                    znam = value;
+                sokr();
+            }
+        }
+        private int nod(int a,int b)
+        {
+            while (b != 0)
+                b = a % (a = b);
+            return a;
+        }
+
+        private void sokr()
+        {
+            var n = nod(chisl, znam);
+            chisl /= n;
+            znam /= n;
+        }
 
         public Fraction(int c, int z)
         {
             chisl = c;
             znam = z;
+            kol++;
+            sokr();
         }
 
-        public Fraction(int c)
+        public Fraction(int c):this(c,1)
         {
-            chisl = c;
-            znam = 1;
+            //chisl = c;
+            //znam = 1;
         }
-        public Fraction()
+        public Fraction():this(0,1)
         {
-            chisl = 0;
-            znam = 1;
+            //chisl = 0;
+            //znam = 1;
         }
 
         public int GetChisl()
@@ -57,6 +103,18 @@ namespace Rational
                 return $"{chisl}";
             else
                 return $"{chisl}/{znam}";
+        }
+
+        public static void StaticMethod(int x)
+        {
+            Console.WriteLine($"Статичный метод {x}");
+
+        }
+
+        public static int CountOfObjects()
+        {
+            return kol;
+
         }
 
 
--- Rational/Rational/Program.cs
+++ Rational/Rational/Program.cs
@@ -14,17 +14,32 @@ namespace Rational
         {
             Fraction f1 = new Fraction();
             Fraction f2 = new Fraction(5);
-            Fraction f = new Fraction(3,4);
+            Fraction f = new Fraction(9,12);
 
             Console.WriteLine($"f={f}");
             Console.WriteLine($"f1={f1}");
             Console.WriteLine($"f2={f2}");
 
-            f.SetChisl(6);
+            //f.SetChisl(6);
+            f.Chisl = 6;
+            f.Znam = 18;
+            //int x = f.Chisl;
 
             Console.WriteLine($"f={f}");
 
+            f.Znam = 0;
             
+            
+            Console.WriteLine($"f={f}");
+
+            Console.WriteLine($"kolvo - {Fraction.CountOfObjects()}");
+            //Fraction.procent = 7;
+
+            Fraction f5 = new Fraction(30, 45);
+            Console.WriteLine($"f5={f5}");
+
+            Console.WriteLine($"kolvo - {Fraction.CountOfObjects()}");
+
 
             //Rational r;
             //r = new Rational(1,0);
